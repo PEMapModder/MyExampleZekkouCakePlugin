1dd234a5d1f4f3325eb7f3935300cf4755444c61
PEMapModder <pemapmodder1970@gmail.com>
2014-03-31 18:50:53 +0800

    Update and rename ExamplePlugins/ExamplePlugin0/MainPlugin.php to ExamplePlugins/ExamplePlugin0/src/ExamplePlugin0/MainPlugin.php

---
+++ ExamplePlugins/ExamplePlugin0/src/ExamplePlugin0/MainPlugin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ExamplePlugins\ExamplePlugin0;
+
+use PocketMine\Utils\TextFormat as Colors;
+
+class MainPlugin{
+	public function onLoad(){
+		PocketMine\console(Colors::AQUA."Loading ExamplePlugin0!");
+	}
+	public function onEnable(){
+		PocketMine\console(Colors::LIGHT_PURPLE."Enabling ExamplePlugin0!");
+	}
+	public function onDisable(){
+		PocketMine\console(Colors::YELLOW."Disabling ExamplePLugin0!");
+	}
+}
