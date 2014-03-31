57bb15f1b1e7342d3b9786dc0398efdc36d9c45f
PEMapModder <pemapmodder1970@gmail.com>
2014-03-31 19:21:12 +0800

    Update MainPlugin.php

--- ExamplePlugins/PingLogger/src/PingLogger/MainPlugin.php
+++ ExamplePlugins/PingLogger/src/PingLogger/MainPlugin.php
@@ -3,8 +3,28 @@
 namespace PEMapModder\ExamplePlugins\PingLogger;
 
 use PocketMine\Server;
+use PocketMine\Player;
 
 class MainPlugin extends PluginBase{
-	public function onEnable{
+	private $server;
+	public function onEnable(){
+		$this->server = Server::getInstance();
+		@mkdir($this->dataFolder."players/");
+		$this->initialize();
+	}
+	public function initialize(){ // init() is a final function already
+		$this->server->schedule(20, array($this, "graph"));
+	}
+	public function graph(){
+		foreach(Player::getAll() as $p){
+			$ping = $p->getLag();
+			$path = $this->dataFolder."players/".strtolower($p->getName()).".txt";
+			$text = "";
+			for($i=0; $i<=$ping; $i+=40){
+				$text .= " ";
+			}
+			$text .= "|\n";
+			file_put_contents($path, $text, FILE_APPEND);
+		}
 	}
 }
