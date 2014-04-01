391862d10d868dc943dca5ec022f6516125a95c7
PEMapModder <pemapmodder1970@gmail.com>
2014-04-01 23:51:51 +0800

    Finished WorldList

--- ExamplePlugins/WList/src/WList/Plugin.php
+++ ExamplePlugins/WList/src/WList/Plugin.php
@@ -1,3 +1,28 @@
 <?php
 
 namespace PEMapModder\WorldList;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as Colors;
+
+class MainPlugin extends PluginBase{
+    public function onCommand(CommandSender $issuer, Command $cmd, $label, array $args){
+        if(!($issuer instanceof Player)){
+            $issuer->sendMessage(Colors::YELLOW."Please run this command in-game."); // lol rub this cmd in gamr
+            return true; // not success but still handled
+        }
+        $list=Player::getAll();
+        $ret="List of players at world ".$issuer->level->getName()." (@totalCnt/".count($list).")";
+        $cnt=0;
+        foreach($list as $p){
+            if($p->level->getName()===$issuer->level->getName()){
+                $ret.=$p->getDisplayName().", ";
+                $cnt++;
+            }
+        }
+        $issuer->sendMessage(substr(str_replace("@totalCnt", "$cnt", $ret), 0, -2);
+    }
+}
+
+#TODO Change spaces to tabs
